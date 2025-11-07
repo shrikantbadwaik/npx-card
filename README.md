c0fcb450aab00cec3d12ae0f7b036eece43e65fa
Shrikant B <shrikant.badwaik@grabtaxi.com>
2025-11-07 16:53:49 +0530

    added business card

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# npx-card
-Use npx shrikant to connect with me 
+# npx-card 💳
+
+> A personal interactive business card that runs in your terminal via npx!
+
+## 🚀 Usage
+
+Want to know more about me? Just run:
+
+```bash
+npx shrikant
+```
+
+That's it! No installation needed.
+
+
+## 💼 About Me
+
+Software Engineer at **Grab Holdings Inc.** 🚗  
+Android Developer | Kotlin Enthusiast | Jetpack Composer
+
+## 🙏 Inspiration
+
+Inspired by the awesome [npx card](https://github.com/bnb/bitandbang) trend in the developer community!
+
+## Steps to create your own business card
+The article written by our friend [@jackboberg](https://github.com/jackboberg). I used the same for the reference to deploy the package. Write a [Simple npx Business Card](https://studioelsa.se/blog/open-source-oss-npx-business-card/).
+
+---
+
+**Note**: Make sure you have Node.js installed to run this card.
---
+++ cli.js
@@ -0,0 +1,116 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const ora = require('ora');
+const cliSpinners = require('cli-spinners');
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:shrikant.badwaik@gmail.com");
+                    console.log("\nDone, see you soon at inbox.\n");
+                }
+            },
+            {
+                name: `Visit my ${chalk.magentaBright.bold("GitHub")}?`,
+                value: () => {
+                    open("https://github.com/shrikantbadwaik");
+                    console.log("\nOpening GitHub profile...\n");
+                }
+            },
+            {
+                name: `Connect on ${chalk.blue.bold("LinkedIn")}?`,
+                value: () => {
+                    open('https://www.linkedin.com/in/shrikant-badwaik/');
+                    console.log("\nOpening LinkedIn profile...\n");
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Hasta la vista.\n");
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: chalk.bold.green("             Shrikant Badwaik"),
+    handle: chalk.white("@shrikantbadwaik"),
+    work: `${chalk.white("Software Engineer at")} ${chalk
+        .hex("#00B14F")
+        .bold("Grab Holdings Inc.")}`,
+    twitter: chalk.gray("https://x.com/") + chalk.cyan("00shrikant00"),
+    github: chalk.gray("https://github.com/") + chalk.green("shrikantbadwaik"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("shrikant-badwaik"),
+    email: chalk.gray("Email: ") + chalk.yellow("shrikant.badwaik@gmail.com"),
+    npx: chalk.red("npx") + " " + chalk.white("shrikant"),
+
+    labelWork: chalk.white.bold("       Work:"),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelEmail: chalk.white.bold("      Email:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        ``,
+        `${data.labelTwitter}  ${data.twitter}`,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelEmail}  ${data.email}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic(
+            "Jetpack-ing my way through Android stories..."
+        )}`,
+        `${chalk.italic("one screen at a time.")}`,
+        ``,
+        `${chalk.italic(
+            "Feel free to reach out - my inbox is always open!"
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }
+);
+
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
